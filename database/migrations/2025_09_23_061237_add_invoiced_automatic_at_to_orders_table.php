9340cfb40b61e15ae5c15edcb6ea87d10aacafa2
IM3-238\im3-div6 <newal.medina@ingenieros-im3.com>
2025-09-23 09:36:52 +0200

    añadiendo campos

--- database/migrations/2025_09_23_061237_add_invoiced_automatic_at_to_orders_table.php
+++ database/migrations/2025_09_23_061237_add_invoiced_automatic_at_to_orders_table.php
@@ -12,6 +12,17 @@ return new class extends Migration
             $table->boolean('invoiced_automatic')
                 ->default(0)
                 ->after('invoiced');
+            $table->dateTime('confirmed_by_amovens')
+                ->nullable()
+                ->after('invoiced_automatic');
+
+            $table->dateTime('cancelled_by_amovens')
+                ->nullable()
+                ->after('confirmed_by_amovens');
+
+            $table->string('amovens_id')
+                ->nullable()
+                ->after('cancelled_by_amovens');
         });
     }
 
@@ -19,6 +30,9 @@ return new class extends Migration
     {
         Schema::table('orders', function (Blueprint $table) {
             $table->dropColumn('invoiced_automatic');
+            $table->dropColumn('confirmed_by_amovens');
+            $table->dropColumn('cancelled_by_amovens');
+            $table->dropColumn('amovens_id');
         });
     }
 };
