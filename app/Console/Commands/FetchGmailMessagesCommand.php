904398bd03b2bf391012abc17587c0af5471a9e0
IM3-238\im3-div6 <newal.medina@ingenieros-im3.com>
2025-09-28 17:37:45 +0200

    no message

--- app/Console/Commands/FetchGmailMessagesCommand.php
+++ app/Console/Commands/FetchGmailMessagesCommand.php
@@ -26,6 +26,7 @@ class FetchGmailMessagesCommand extends Command
             $this->info("Se encontraron " . count($messages) . " mensajes recientes:");
 
             foreach ($messages as $msg) {
+                $data = $this->getInfo($msg);
 
                 if (empty($data['matricula_coche'] ?? null)) {
                     $this->info("No se encontró matrícula en el mensaje, se omite.");
